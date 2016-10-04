577b9c1564c84595c4e0fcc9aa16963fa3f0f051
Vitalii Doroshenko <vetalpro.exe@gmail.com>
2016-10-04 19:28:57 +0300

    add settings for singularity and breakpoint

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
   $.gulp.task('sass', function() {
-    return $.gulp.src('./source/style/*.sass')
+    return $.gulp.src('./source/style/*.{sass,scss}')
       .pipe($.gp.sourcemaps.init())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@
 module.exports = function() {
   $.gulp.task('watch', function() {
     $.gulp.watch('./source/js/**/*.js', $.gulp.series('js:process'));
-    $.gulp.watch('./source/style/**/*.sass', $.gulp.series('sass'));
+    $.gulp.watch('./source/style/**/*.{sass,scss}', $.gulp.series('sass'));
     $.gulp.watch('./source/jade/**/*.pug', $.gulp.series('pug'));
     $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy:image'));
     $.gulp.watch('./source/fonts/**/*.*', $.gulp.series('copy:fonts'));
