cdefa4c1c26a50af60a6bb34276f88bb8531133e
Vitalii Doroshenko <vetalpro.exe@gmail.com>
2016-10-15 08:40:21 +0300

    uglify for browserify

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -2,14 +2,17 @@
 
 module.exports = function () {
   $.gulp.task('browserify', function () {
-    return $.browserify('source/js/app.js', {debug: true})
+    return $.browserify({
+          entries:'source/js/app.js',
+          debug: true
+        })
         .bundle()
         .pipe($.source('script.js'))
         //TODO:uglify browserify
-        // .pipe($.buffer())
-        // .pipe($.gp.sourcemaps.init({loadMaps: true}))
-        // .pipe($.gp.uglify())
-        // .pipe($.gp.sourcemaps.write())
+        .pipe($.buffer())
+        .pipe($.gp.sourcemaps.init({loadMaps: true}))
+        .pipe($.gp.uglify())
+        .pipe($.gp.sourcemaps.write())
         .pipe($.gulp.dest($.config.root + '/js'));
   });
 };
