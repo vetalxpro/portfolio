20585cbbf5cf20235240d700402fa5487f90856f
vetalxpro <vetalpro.exe@gmail.com>
2016-10-28 11:17:15 +0300

    end

--- gulp/tasks/js.lint.js
+++ gulp/tasks/js.lint.js
@@ -4,6 +4,6 @@ module.exports = function() {
   $.gulp.task('js:lint', function() {
     return $.gulp.src($.path.app)
       .pipe($.gp.eslint())
-      .pipe($.gp.eslint.format());
+      .pipe($.gp.eslint.format());;
   });
 };
